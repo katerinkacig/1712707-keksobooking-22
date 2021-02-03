fc4e48e208d3cb4adbeed3c11f83811849fc3516
Ekaterina <ciganovaekaterina@gmail.com>
2021-02-03 14:00:43 +0300

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-let getRandomInt = function (min, max) {
+const getRandomInt = function (min, max) {
   if (min > max) {
-    [min, max] = [max, min];
+    throw new RangeError('Неправильный диапозон ('+ min + ', ' + max + ') . Максимальное значение диапазона должно быть больше минимального')
+  }
+  if (min === max) {
+    throw new RangeError('Неправильный диапозон ('+ min + ', ' + max + ') . Минимальное и максимальное значения не должны быть равны')
+  }
+  if (min < 0 || max < 0) {
+    throw new RangeError('Неправильный диапозон ('+ min + ', ' + max + ') . Диапазон может быть только положительный, включая ноль')
   }
 
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min < 0) {
-    min = 0;
-  }
-
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+getRandomInt(0, 10);
 
-let getRandomFloat = function (min, max, decimalPlaces) {
+const getRandomFloat = function (min, max, decimalPlaces) {
   if (min > max) {
-    [min, max] = [max, min];
+    throw new RangeError('Неправильный диапозон ('+ min + ', ' + max + ') . Максимальное значение диапазона должно быть больше минимального')
   }
-
-  if (min < 0) {
-    min = 0;
+  if (min === max) {
+    throw new RangeError('Неправильный диапозон ('+ min + ', ' + max + ') . Минимальное и максимальное значения не должны быть равны')
+  }
+  if (min < 0 || max < 0) {
+    throw new RangeError('Неправильный диапозон ('+ min + ', ' + max + ') . Диапазон может быть только положительный, включая ноль')
   }
 
   return parseFloat((Math.random() * (max - min) + min).toFixed(decimalPlaces));
 }
+
+getRandomFloat(1.1, 1.2, 3);
